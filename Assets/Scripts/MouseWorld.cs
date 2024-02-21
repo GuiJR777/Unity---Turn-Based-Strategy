19fc900508ef2344823d155beeb1683e0a2781c8
Guilherme Jesuino Ramires <guilherme.ramires@quintoandar.com.br>
2024-02-21 00:53:57 -0300

    ✨feature: Add a mouse raycast

---
+++ Assets/Scripts/MouseWorld.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseWorld : MonoBehaviour
+{
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Debug.Log(Physics.Raycast(ray));
+
+    }
+}
